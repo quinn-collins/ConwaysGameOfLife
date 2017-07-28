ba4953cbb3963df8e362a4548122cb2bdb6292a7
Quinn Collins <collinsqui@gmail.com>
2017-07-28 15:29:30 -0400

    got the count working on number of cells surrounding each cell

--- src/main/java/GameOfLife.java
+++ src/main/java/GameOfLife.java
@@ -25,7 +25,17 @@ public class GameOfLife {
         gameGrid[5][3] = true;
         gameGrid[5][4] = true;
         
-        // print game grid
+        printOutCurrentGameGrid(gameGrid, vertical, horizontal);
+        
+        moveToNextGeneration(gameGrid, vertical, horizontal);
+        
+        
+        printOutCurrentGameGrid(gameGrid, vertical, horizontal);
+		
+	}
+	
+	// print game grid
+	public static void printOutCurrentGameGrid(boolean[][] gameGrid, int vertical, int horizontal) {
         for (int i = 0; i < vertical; i++) {
             for (int j = 0; j < horizontal; j++) {
             	if(gameGrid[i][j]) {
@@ -37,6 +47,50 @@ public class GameOfLife {
             }
             System.out.println();
         }
-		
 	}
+	
+	//get a count for number of live cells around each cell
+	public static int getCountOfLifeCellsAroundEachCell(boolean[][] gameGrid, int i, int j) {
+		int count = 0;
+		
+		if(i>0 && j>0 && gameGrid[i-1][j-1]) {
+			count++;
+		}
+		if(i>0 && gameGrid[i-1][j]) {
+			count++;
+		}
+		if(j<gameGrid[0].length-1 && i>0 && gameGrid[i-1][j+1]) {
+			count++;
+		}
+		if(j>0 && gameGrid[i][j-1]) {
+			count++;
+		}
+		if(j<gameGrid[0].length-1 && gameGrid[i][j+1]) {
+			count++;
+		}
+		if(i<gameGrid.length-1 && j>0 && gameGrid[i+1][j-1]) {
+			count++;
+		}
+		if(i<gameGrid.length-1 && gameGrid[i+1][j]) {
+			count++;
+		}
+		if(i<gameGrid.length-1 && j<gameGrid.length-1 && gameGrid[i+1][j+1]) {
+			count++;
+		}
+		return count;
+	}
+	
+	//let logic decide which cells die and which live to move to next generation
+	public static void moveToNextGeneration(boolean[][] gameGrid, int vertical, int horizontal) {
+		for(int i = 0; i < vertical; i++) {
+        	for(int j = 0; j < horizontal; j++) {
+        		System.out.println(i + "" + j);
+        		int count = getCountOfLifeCellsAroundEachCell(gameGrid, i, j);
+        		if(gameGrid[i][j] && count <= 2) {
+        			gameGrid[i][j] = false;
+        		}
+        	}
+        }
+	}
+	
 }
